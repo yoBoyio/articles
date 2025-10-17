0a0fd2aa61da70409555fd1b55e524130e59e787
yoBoyio <dimi044@hotmail.com>
2025-10-17 12:59:38 +0200

    fix(frontend):  protected routes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
+import HomePage from '../pages/HomePage';
+import ArticleForm from '../components/ArticleForm';
 import ProtectedRoute from '../components/ProtectedRoute';
 
 const AppRoutes = () => {
@@ -8,6 +10,30 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route 
+        path="/" 
+        element={
+          <ProtectedRoute>
+            <HomePage />
+          </ProtectedRoute>
+        } 
+      />
+      <Route 
+        path="/articles/create" 
+        element={
+          <ProtectedRoute>
+            <ArticleForm />
+          </ProtectedRoute>
+        } 
+      />
+      <Route 
+        path="/articles/edit/:id" 
+        element={
+          <ProtectedRoute>
+            <ArticleForm />
+          </ProtectedRoute>
+        } 
+      />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
